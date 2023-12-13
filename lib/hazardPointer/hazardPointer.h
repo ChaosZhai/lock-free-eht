adde13dff31142b8742eaca3178825c3150e694b
chaoszhai <ruijiez@andrew.cmu.edu>
2023-12-13 18:21:00 -0500

    hazard pointer and lockfree

---
+++ lib/hazardPointer/hazardPointer.h
@@ -0,0 +1,37 @@
+//
+// Created by Chaos Zhai on 12/13/23.
+//
+#pragma once
+
+#include "reclaimer.h"
+
+class HazardPointer {
+public:
+    HazardPointer() : reclaimer_(nullptr), index(Reclaimer::HP_INDEX_NULL) {}
+    HazardPointer(Reclaimer* reclaimer_, void* ptr)
+            : reclaimer_(reclaimer_), index(reclaimer_->MarkHazard(ptr)) {}
+
+    ~HazardPointer() { UnMark(); }
+
+    HazardPointer(const HazardPointer& other) = delete;
+    HazardPointer(HazardPointer&& other) noexcept {
+        *this = std::move(other);
+    }
+
+    HazardPointer& operator=(const HazardPointer& other) = delete;
+    HazardPointer& operator=(HazardPointer&& other)  noexcept {
+        reclaimer_ = other.reclaimer_;
+        index = other.index;
+
+        // If move assign is called, we must disable the other's index to avoid
+        // incorrectly unmark index when other destruct.
+        other.index = Reclaimer::HP_INDEX_NULL;
+        return *this;
+    }
+
+    void UnMark() const { reclaimer_->UnMarkHazard(index); }
+
+public:
+    Reclaimer* reclaimer_{};
+    int index{};
+};
---
+++ lib/hazardPointer/internalHazardPointer.h
@@ -0,0 +1,50 @@
+//
+// Created by Chaos Zhai on 12/13/23.
+//
+#include <atomic>
+
+/**
+ * InternalHazardPointer is used to store the hazard pointer.
+ * It is a node of HazardPointerList.
+ */
+class InternalHazardPointer {
+public:
+    InternalHazardPointer() : ptr(nullptr), next(nullptr) {}
+    ~InternalHazardPointer() = default;
+
+    // not copy-able
+    // delete move constructors
+    InternalHazardPointer(const InternalHazardPointer& other) = delete;
+    InternalHazardPointer(InternalHazardPointer&& other) = delete;
+    InternalHazardPointer& operator=(const InternalHazardPointer& other) = delete;
+    InternalHazardPointer& operator=(InternalHazardPointer&& other) = delete;
+
+    // all fields atomic to ensure thread safety
+    std::atomic_flag flag{};
+    std::atomic<void*> ptr;
+    std::atomic<InternalHazardPointer*> next;
+};
+
+/**
+ * HazardPointerList is a list of InternalHazardPointer.
+ * It is used to store all hazard pointers.
+ * It will free all InternalHazardPointer memory upon destruction.
+ */
+class HazardPointerList {
+public:
+    HazardPointerList() : head(new InternalHazardPointer()), size(0) {}
+    ~HazardPointerList() {
+        // HazardPointerList destruct when program exit.
+        InternalHazardPointer* p = head.load(std::memory_order_acquire);
+        while (p) {
+            InternalHazardPointer* temp = p;
+            p = p->next.load(std::memory_order_consume);
+            delete temp;
+        }
+    }
+
+    size_t get_size() const { return size.load(std::memory_order_consume); }
+
+    std::atomic<InternalHazardPointer*> head;
+    std::atomic<int> size;
+};
