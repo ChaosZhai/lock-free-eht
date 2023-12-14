eee8bb9abee88cf8c6e25de1cfde0613c679fed5
yicheng4 <yicheng4@andrew.cmu.edu>
2023-12-14 14:51:18 -0500

    fine lock

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "include/eht.h"
 
 #include "include/coarse-eth.h"
+#include "include/fine-eth.h"
 #include "lib/comparator/int-comparator.h"
 
 #define ASSERT_TRUE(condition) assert(condition)
@@ -20,7 +21,7 @@
 
 using namespace eht;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     auto hash_fn = HashFunction<int>();
     auto cmp = IntComparator();
     auto eht = CoarseEHT<int, int, IntComparator>("test", cmp, hash_fn);
@@ -47,10 +48,10 @@ int main() {
     }
 
     for (int i = 1 << 30; i < num_keys + (1 << 30); i++) {
-        bool inserted = eht.Insert(i, i);
+        bool inserted = eht2.Insert(i, i);
         ASSERT_TRUE(inserted);
         auto res =
-        eht.Get(i);
+                eht2.Get(i);
         ASSERT_EQ(i, *res);
     }
 
@@ -65,7 +66,7 @@ int main() {
     }
 
     for (int i = 1 << 30; i < num_keys + (1 << 30); i++) {
-       auto res = eht.Get(i);
+       auto res = eht2.Get(i);
         ASSERT_TRUE(res.has_value());
 
         ASSERT_EQ(i, *res);
@@ -80,6 +81,63 @@ int main() {
 
     }
 
+    auto eht3 = FineEHT<int, int, IntComparator>("test3", cmp, hash_fn);
+    num_keys = 8;
 
+    // insert some values
+    for (int i = 0; i < num_keys; i++) {
+        bool inserted = eht3.Insert(i, i);
+        assert(inserted);
+        auto res = eht3.Get(i);
+        assert(i == *res);
+    }
+
+    num_keys = 10;
+    auto eht4 = FineEHT<int, int, IntComparator>("test4", cmp, hash_fn);
+
+    // insert some values
+    for (int i = 0; i < num_keys; i++) {
+        bool inserted = eht4.Insert(i, i);
+        ASSERT_TRUE(inserted);
+        auto res = eht4.Get(i);
+
+        ASSERT_EQ(i, *res);
+    }
+
+    for (int i = 1 << 30; i < num_keys + (1 << 30); i++) {
+        bool inserted = eht4.Insert(i, i);
+        ASSERT_TRUE(inserted);
+        auto res =
+                eht4.Get(i);
+        ASSERT_EQ(i, *res);
+    }
+
+
+
+    // check that they were actually inserted
+    for (int i = 0; i < num_keys; i++) {
+
+        auto res = eht4.Get(i);
+
+        ASSERT_EQ(i, *res);
+    }
+
+    for (int i = 1 << 30; i < num_keys + (1 << 30); i++) {
+        auto res = eht4.Get(i);
+        ASSERT_TRUE(res.has_value());
+
+        ASSERT_EQ(i, *res);
+    }
+
+
+
+    // try to get some keys that don't exist/were not inserted
+    for (int i = num_keys; i < 2 * num_keys; i++) {
+        auto res = eht4.Get(i);
+        ASSERT_FALSE(res.has_value());
+
+    }
+
+    std::cout << "All tests passed!" << std::endl;
     return 0;
 }
